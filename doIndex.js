1023a8b48b462a43ec722a5242f9ea0b4a873d50
gladiatorthemes <80295200+gladiatorthemes@users.noreply.github.com>
2021-03-09 15:05:59 +0200

    commit

--- doIndex.js
+++ doIndex.js
@@ -1,5 +1,6 @@
 
 const mongodb = require("mongodb");
+const http = require('http');
 const uri = "mongodb://admin:Prince1512@104.197.54.59:27017/"; 
 
 mongodb.MongoClient.connect(uri, async(err, db) => {
@@ -7,36 +8,44 @@ mongodb.MongoClient.connect(uri, async(err, db) => {
         console.log(err);
     }
     console.log("Start - " + new Date());
-    var dbo = db.db("checkthisproperty");
+    let dbo = db.db("checkthisproperty");
     let c = 0;
-    let result = await dbo.collection("adhoc").findOneAndDelete({});
-    let add = await dbo.collection("address").find().skip(result.value.offset).limit(10000);
-    add.each(async function(err, doc) {
-        c++;
-     
-       // let rr = dbo.collection('addressMain').insertOne({"test":1});
-        for(i=1; i<38; i++){
-            
-            var r = await dbo.collection("Lot_"+i).findOne({
-                geometry: {
-                        $geoIntersects: {
-                            $geometry: {
-                                type: "Point",
-                                coordinates: doc.coordinates,
-                            }
-                        }
-                    }
-                });
-                if(r!=null && doc!=null){
-                    console.log("Done - " + new Date());
-                    let tt = {};
-                    tt.itslotid = r.properties.itslotid;
-                    tt.address = doc.address;
-                    tt.coordinates = doc.coordinates;
-                    tt.type = doc.type;
-                    tt.lot = r.geometry;
-                    dbo.collection('addressMain').insertOne(tt);     
-                }          
+    let res1 = await dbo.collection("adhoc").findOneAndDelete({});
+    let add =  dbo.collection("address").find().skip(res1.value.offset).limit(10000).toArray(function(err, result) {
+        if (err) throw err;
+        if(result !=null){  
+           
+            result.forEach(async doc => {
+                c++;          
+                     var document = await dbo.collection("Lot").findOne({
+                         geometry: {
+                                 $geoIntersects: {
+                                     $geometry: {
+                                         type: "Point",
+                                         coordinates: doc.coordinates,
+                                     }
+                                 }
+                             }
+                         });
+                     
+                         if(document!=null){
+                           
+                             let tt = {};
+                             tt.itslotid = document.properties.itslotid;
+                             tt.address = doc.address;
+                             tt.coordinates = doc.coordinates;
+                             tt.type = doc.type;
+                             tt.lot = document.geometry;
+                             //console.log(tt);
+                            http.get('http://104.197.54.59:8580/insert?data='+JSON.stringify(tt), (resp) => {});
+                             
+                             console.log("Stop - " + new Date());
+                         }
+            })
         }
-    })
-});
+       
+      });
+
+    });
+  
+
